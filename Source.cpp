8107dd945b0b11db8bf02207de0fa0dc014ed795
CHONG PEI WAEN <TP051165@mail.apu.edu.my>
2021-04-08 21:54:36 +0800

    Beep boop

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS //source: https://stackoverflow.com/questions/22450423/how-to-use-crt-secure-no-warnings (if not cant use localtime since its depreciated. Need to use localtime_s instead
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <iomanip>
 #include <sstream>
+#include <ctime>
+#include <stdlib.h>
 using namespace std;
 
 //////////////////////////// End of top sections ////////////////////////////
@@ -17,13 +20,14 @@ public:
 	string publisher;
 	float bP;
 	string genre;
+	string date;
 	int stock = 0;
 	ofstream booksFile;
 	stringstream streambP;
 	void addBooks();
 	void readFromFile();
 	void displayList();
-
+	string getDate();
 	Books* next;
 };
 
@@ -32,6 +36,18 @@ public:
 struct Books* head = NULL;
 
 //////////////////////////////////////////////////////////////////////////
+
+//source code: https://ideone.com/r3SlQL + https://stackoverflow.com/questions/27934034/get-current-time-in-mm-dd-yyyy-in-c
+
+string Books::getDate() {
+	const int MAXLEN = 80;
+	char s[MAXLEN];
+	time_t t = time(0);
+	strftime(s, MAXLEN, "%m/%d/%Y", localtime(&t));
+	return s;
+}
+
+//////////////////////////// End of getDate ////////////////////////////
 
 void Books::addBooks() {
 	//set new stream, set it to 2 dp, input double/float to stream, change stream to string
@@ -61,10 +77,12 @@ void Books::addBooks() {
 	}
 	cout << "Enter genre of book: ";
 	cin >> genre;
+	
+	date = getDate();
 
 	streambP << fixed << setprecision(2) << bP;
 	string bookPrice = streambP.str();
-	string bookDetail = title + "|" + to_string(ISBN) + "|" + author + "|" + publisher + "|" + bookPrice + "|" + to_string(stock) + "|" + genre;
+	string bookDetail = title + "|" + to_string(ISBN) + "|" + author + "|" + publisher + "|" + bookPrice + "|" + to_string(stock) + "|" + genre + "|" + getDate();
 
 	//append: write new line
 	booksFile.open("books.txt", ios::app);
@@ -83,34 +101,6 @@ void Books::addBooks() {
 
 //////////////////////////// end of AddBook ////////////////////////////
 
-//void Books::insertBookToList(string t, int i, string a, string p, float b, int s, string g) {
-//
-//	struct Books* new_Book = (struct Books*)malloc(sizeof(struct Books));
-//
-//	new_Book->title = t;
-//	new_Book->ISBN = i;
-//	new_Book->author = i;
-//	new_Book->publisher = p;
-//	new_Book->bP = b;
-//	new_Book->stock = s;
-//	new_Book->genre = g;
-//
-//	new_Book->next = NULL;
-//
-//	if (head == NULL) {
-//		head = new_Book;
-//	}
-//	else {
-//		Books* last = head;
-//		while (last->next != NULL) {
-//			last = last->next;
-//		}
-//		last->next = new_Book;
-//	}
-//}
-
-//////////////////////////// end of insertBookAtList ////////////////////////////
-
 void Books::readFromFile() {
 
 	ifstream booksFile("books.txt");
@@ -124,20 +114,19 @@ void Books::readFromFile() {
 	string genres;
 	
 	string line;
-	int number = 0;
 
 	if (booksFile.is_open()) {
 
 		while (getline(booksFile,line)) {
 			stringstream ss(line);
-			number++;
-			getline(ss, titles, '|'); 
+			getline(ss, title, '|'); 
 			getline(ss, ISBNs, '|');
-			getline(ss, authors, '|');
-			getline(ss, publishers, '|');
+			getline(ss, author, '|');
+			getline(ss, publisher, '|');
 			getline(ss, bPs, '|');
 			getline(ss, stocks, '|');
-			getline(ss, genres, '|');
+			getline(ss, genre, '|');
+			getline(ss, date, '|');
 
 			//use new instead of malloc, if not wont be able to read string //checkout this source https://www.programmersought.com/article/87014391850/ 
 			//+ https://www.geeksforgeeks.org/new-vs-malloc-and-free-vs-delete-in-c/#:~:text=malloc()%3A%20It%20is%20a,%E2%80%9Cmalloc()%E2%80%9D%20does%20not.
@@ -146,12 +135,13 @@ void Books::readFromFile() {
 			struct Books* new_Book = new Books;
 
 			new_Book->ISBN = stoi(ISBNs); //stoi = string to integer
-			new_Book->title = titles;
-			new_Book->author = authors;
-			new_Book->publisher = publishers;
+			new_Book->title = title;
+			new_Book->author = author;
+			new_Book->publisher = publisher;
 			new_Book->bP = stof(bPs); //stof = string to float
 			new_Book->stock = stoi(stocks);
-			new_Book->genre = genres;
+			new_Book->genre = genre;
+			new_Book->date = date;
 
 			new_Book->next = NULL;
 
@@ -166,7 +156,6 @@ void Books::readFromFile() {
 				last->next = new_Book;
 			}
 			
-			//cout << titles + "|" + ISBNs + "|" + authors + "|" + publishers + "|" + bPs + "|" + stocks + "|" + genres << endl;
 		}
 
 		booksFile.close();
@@ -180,14 +169,17 @@ void Books::readFromFile() {
 //////////////////////////// end of readFromFile ////////////////////////////
 
 void Books::displayList() {
+	int noBooks = 0;
 	struct Books* ptr;
 	ptr = head;
+	cout << "*--------------- Book List ---------------*" << endl;
 	while (ptr != NULL) {
+		noBooks++;
+		cout << ptr->title << "|" << ptr->ISBN << "|" << ptr->author << "|" << ptr->publisher << "|" << ptr->bP << "|" << ptr->stock << "|" << ptr->genre << "|" << ptr->date << endl;\
 		cout << "*--------------------------------------------------*" << endl;
-		cout << ptr->title << "|" << ptr->ISBN << "|" << ptr->author << "|" << ptr->publisher << "|" << ptr->bP << "|" << ptr->stock << "|" << ptr->genre << endl;
 		ptr = ptr->next;
 	}
-	
+	cout << "Done displaying all books. There are a total of " << noBooks << " different books inside the database." <<endl;
 }
 
 //////////////////////////// end of displayList ////////////////////////////
@@ -196,19 +188,19 @@ int main() {
 
 	//declareables 
 	Books books;
-	int rpt;
 
 	//Main Menu
 	int choice;
 
-	MainMenu:
+MainMenu:
+	cout << "*--------------------------------------------------*" << endl;
 	cout << "Welcome to Kinokuniya Book Inventory Management System!" << endl;
 	cout << "*--------------------------------------------------*" << endl; // 50s -
 	cout << "Which section do you want to access:\n1. Inventory\n2. Purchase\n3. Exit" << endl;
 	cout << "Enter choice: ";
 	cin >> choice;
 
-	while (choice !=1 && choice !=2)
+	while (choice < 1 || choice > 3)
 	{
 		cout << "*--------------------------------------------------*" << endl;
 		cout << "ERROR! Value entered not recognized!\nWhich section do you want to access:\n1. Inventory\n2. Purchase" << endl;
@@ -222,7 +214,7 @@ int main() {
 	{
 	case 1:
 		
-		Menu:
+		InventoryMenu:
 		cout << "*--------------- Inventory Section ---------------*" << endl;
 		cout << "Which section do you want to access:\n1. Add Books\n2. View Books\n3. Search Books \n4. Filter Books\n5. Sort Books\n6. Main Menu" << endl;
 		cout << "Enter choice: ";
@@ -246,21 +238,23 @@ int main() {
 			books.addBooks();
 			cout << "*--------------------------------------------------*" << endl;
 			cout << "Would you like to add more books? \n1. Yes\n2. No\nEnter choice: ";
-			cin >> rpt;
-			while (rpt == 1) {
+			cin >> choice;
+			while (choice == 1) {
 				books.addBooks();
 				cout << "*--------------------------------------------------*" << endl;
 				cout << "Would you like to add more books? \n1. Yes\n2. No\nEnter choice: ";
-				cin >> rpt;
+				cin >> choice;
 				}
 
-			goto Menu;
+			goto InventoryMenu;
 
 			break;
 		case 2:
 			books.readFromFile();
 			books.displayList();
 			cout << "*--------------------------------------------------*" << endl;
+			cout << "Back to Inventory Menu" << endl;
+			goto InventoryMenu;
 			break;
 		case 3:
 			break;
@@ -310,11 +304,39 @@ int main() {
 			break;
 		}
 
+		break;
 
+		//////////////////////////// End of Purchase Section ////////////////////////////
+		
+	case 3:
+		cout << "*--------------------------------------------------*" << endl;
+		cout << "Are you sure you want to exit this program? \n1. Yes\n2. No" << endl;
+		cout << "Enter choice: ";
+		cin >> choice;
+		while (choice < 1 || choice > 2)
+		{
+			cout << "*--------------------------------------------------*" << endl;
+			cout << "ERROR! Value entered not recognized!\nAre you sure you want to exit this program? \n1. Yes\n2. No" << endl;
+			cout << "Enter choice: ";
+			cin >> choice;
+		}
+
+		switch (choice) {
+		case 1:
+			cout << "*--------------------------------------------------*" << endl;
+			cout << "Thank you. See you next time!" << endl;
+			cout << "Exiting program......" << endl;
+			exit(0); //source: https://www.tutorialspoint.com/c_standard_library/c_function_exit.htm
+			break;
+		case 2:
+			goto MainMenu;
+			break;
+		}
+
+		//////////////////////////// End of Exit ////////////////////////////
 
 		break;
+
+		//////////////////////////// End of Main Menu ////////////////////////////
 	}
-
-	//////////////////////////// End of Purchase Section ////////////////////////////
-
 }
